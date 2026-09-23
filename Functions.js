6e390cc1f90f9b34a992be602c2cbde1bf2d755a
ashumane-crypto <ashumane2005@gmail.com>
2026-09-23 22:47:21 +0530

    Added Function Types

---
+++ Functions.js
@@ -0,0 +1,12 @@
+//Function without Parameters
+function greet(){  // Function Declaration
+    console.log("Hello World");
+ } // Function Body
+
+
+// 2.Function with Parameters
+function square(num){  // Function Declaration
+    console.log(num*num); // Function Body
+
+}
+square(5);// Function Call
