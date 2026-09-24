aac8de105c2f28dd849e418e00716020b948a9d7
ashumane-crypto <ashumane2005@gmail.com>
2026-09-24 22:14:20 +0530

    Added multiple parameter and return function

--- Functions.js
+++ Functions.js
@@ -1,7 +1,8 @@
 //Function without Parameters
 function greet(){  // Function Declaration
-    console.log("Hello World");
- } // Function Body
+    console.log("Hello World"); // Function Body
+ }
+ greet()
 
 
 // 2.Function with Parameters
@@ -10,3 +11,16 @@ function square(num){  // Function Declaration
 
 }
 square(5);// Function Call
+
+// 3.Multiple Parameters
+function add(a,b){
+    console.log(a+b);
+}
+add(5,9);
+
+// 4.Return statement
+function multi(a1,b1){
+    return a1*b1;
+}
+let result=multi(5,6);
+console.log(result);
