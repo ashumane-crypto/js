d71e3f6b68b2ea47dc4173d990837a0290c6f4a6
ashumane-crypto <ashumane2005@gmail.com>
2026-09-24 22:28:24 +0530

    Added Arrays

---
+++ Array.js
@@ -0,0 +1,8 @@
+let fruits=["Apple","Banana","Mango","Orange"];
+console.log(fruits);
+
+let numbers=[1,2,3,4,5];
+numbers.push(9);
+console.log(numbers);
+console.log(numbers.length);
+console.log(numbers[2]);
