f72b66328fd7cee46dd5d15557c822e7ea3f93b8
ashumane-crypto <ashumane2005@gmail.com>
2026-09-26 22:16:19 +0530

    Methods of arrays are added

--- Array.js
+++ Array.js
@@ -1,5 +1,10 @@
 let fruits=["Apple","Banana","Mango","Orange"];
 console.log(fruits);
+fruits.pop(); //removes the last element from the array
+fruits.shift(); //removes the first element from the array
+fruits.unshift("Grapes"); //adds an element at the beginning of the array
+console.log(indexof("Mango")); //returns the index of the element in the array
+
 
 let numbers=[1,2,3,4,5];
 numbers.push(9);
